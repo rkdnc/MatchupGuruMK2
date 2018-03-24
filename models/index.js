6fe86b7c9491ef8c6c8e8283be3d0b2d3fa0c8c6
Jeff Nabors <bulletr0k@gmail.com>
2018-03-24 10:09:44 -0400

    Set up environmental variables for MySQL. Attempting to push to Heroku.

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-	User:require("./User.js")
+	//User:require("./Gurus.js")
 	//require other models
 }
 //add more models as you create them
--- server.js
+++ server.js
@@ -2,22 +2,27 @@ const express = require("express");
 const mysql = require("mysql");
 const app = express();
 const PORT = process.env.PORT || 3001;
-const models = require("./models");
-const keys = require("./config/keys").dbpw;
-
+// const models = require("./models");
+const dotenv = require("dotenv").config();
 
 //DB connection
 
 const connection = mysql.createConnection({
   host: "localhost",
-  user:"root",
-  password: keys,
-  database: "matchup_guru"
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
 });
 
-connection.connect();
-
-//Add models here?
+//MySQL connection
+connection.connect(function(err) {
+  if (err) {
+    console.error(`error connecting: ${err.stack}`);
+    return;
+  }
+ 
+  console.log(`You have connected to the database with threadID ${connection.threadId}`);
+});
 
 const passport = require("passport");
 const session = require('express-session');
@@ -28,19 +33,19 @@ app.use(express.json());
 // Serve up static assets
 app.use(express.static("client/build"));
 
-//  // For Passport
-// Commented out due to mern-mvc only supporting MongoDB and not MySQL
-// app.use(session({ secret: 'changeThis',resave: true, saveUninitialized:true})); // session secret
-// app.use(passport.initialize());
-// app.use(passport.session()); // persistent login sessions
-// require('./config/passport.js')(passport,models.User);
+//Import Passport routes here
 
-// // Add routes, both API and view
-// const routes = require("./routes")(passport);
-// app.use('/',routes);
+/*For Passport
+Commented out due to mern-mvc only supporting MongoDB and not MySQL
+app.use(session({ secret: 'changeThis',resave: true, saveUninitialized:true})); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+require('./config/passport.js')(passport,models.User);
 
+Add routes, both API and view
+const routes = require("./routes")(passport); */
 
-
+//Add all other routes here
 
 // Start the API server
 app.listen(PORT, function() {
