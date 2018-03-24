fcbddfbd831f3288b03925d55888837ff53cf8a9
Jeff Nabors <bulletr0k@gmail.com>
2018-03-24 10:26:38 -0400

    JawsDB information

--- server.js
+++ server.js
@@ -6,13 +6,18 @@ const PORT = process.env.PORT || 3001;
 const dotenv = require("dotenv").config();
 
 //DB connection
+let connection;
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME
-});
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(processs.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host:'localhost',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+  });
+};
 
 //MySQL connection
 connection.connect(function(err) {
