48cbb61461ad53622d547fb03aa5970026256bea
Jeff Nabors <bulletr0k@gmail.com>
2018-04-05 19:19:26 -0400

    Getting some routes functional

--- controllers/results.js
+++ controllers/results.js
@@ -12,8 +12,7 @@ module.exports = {
                 season: req.params.season
             }
         }).then(dbFormat => {
-            let results = dbFormat;
-            res.send(results);
+            res.json(dbFormat);
         })
 
         //for each results in format
@@ -21,28 +20,45 @@ module.exports = {
         //send the result with the most entries
         //send an object of each deck's name with the amount of wins to be put into a chart by ChartJS
         //Render all decks with win % in a list
+    },
+    findByUser: function(req, res) {
+        //Takes the current user and finds all results for that user and format
+        db.Result.findAll({
+            where: {
+                user: req.params.user,
+                format: req.params.format
+            },
+            order: [['updatedAt', 'DESC']]
+        }).then(dbUserResults => {
+            let results = dbUserResults;
+            res.send(results);
+        })
+    },
+    createResult: function(req, res) {
+        const deckName = req.body.deckName;
+        const oppDeckName = req.body.oppDeckName;
+        const format = req.params.format;
+        const wins = req.body.wins;
+        const losses = req.body.losses;
+        const draw = req.body.draw;
+        const user = req.body.user;
+        const eventType = req.body.eventType;
+        const season = req.params.season;
+        db.Result.create({
+            deckName: deckName,
+            oppDeckName: oppDeckName,
+            format: format,
+            wins: wins,
+            losses: losses,
+            draw: draw,
+            user: user,
+            eventType: eventType,
+            season: season
+        }).then(dbResult => res.send(dbResult))
+        //takes the passed in information and creates a new result
+        //Will probably need to be the bulkCreate from Sequelize
+        //accepts multple form entries to assign data from fields to be sent to the database
     }
-    // findByUser: (format, user, req, res) => {
-    //     //Takes the current user and finds all results for that user and format
-    //     db.findAll({
-    //         where: {
-    //             format: req.body.format,
-    //             user: req.body.user
-    //         }
-    //     }).then(dbUserResults => {
-    //         let results = dbUserResults;
-    //         res.send(results);
-    //         //for the current user
-    //         //Display all results where the username of the entry result matches the currently logged in user
-    //         //order by newest first
-    //     })
-    // },
-    // // createResult: (format, req, res) => {
-    // //     console.log(res)
-    // //     //takes the passed in information and creates a new result
-    // //     //Will probably need to be the bulkCreate from Sequelize
-    // //     //accepts multple form entries to assign data from fields to be sent to the database
-    // // },
     // //Update could go here, but we won't allow users to edit records yet
     // delete: (req, res) => {
     //     //deletes record based on ID
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -5,5 +5,7 @@ const router = require('express').Router();
 
 
 module.exports = app => {
-    app.get('/api/results/:format/:season/', controllers.Results.findByformat);
+    app.get('/api/results/:format/:season', controllers.Results.findByformat);
+    app.post('/results/create/:format/:season', controllers.Results.createResult);
+    app.get('/stats/:user/:format', controllers.Results.findByUser)
 };
