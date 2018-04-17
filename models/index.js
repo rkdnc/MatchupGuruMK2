10f307d72497a2eb44c1e1ef37a263c8fef19c30
Jeff Nabors <bulletr0k@gmail.com>
2018-04-16 21:26:01 -0400

    More heroku debugging

--- models/index.js
+++ models/index.js
@@ -10,7 +10,9 @@ const config = require('../config/connection')[env];
 var db        = {};
 
 if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+} else if (process.env.JAWSDB_URL) {
+  var sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
