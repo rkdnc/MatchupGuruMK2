b18d3987c66f752fa68419438b093f6ba86c3291
Jeff Nabors <bulletr0k@gmail.com>
2018-04-16 21:57:31 -0400

    Heroku

--- models/index.js
+++ models/index.js
@@ -9,14 +9,13 @@ var env       = process.env.NODE_ENV || 'development';
 var config = require('../config/connection')[env];
 var db        = {};
 
-// if (config.use_env_variable) {
-//   var sequelize = new Sequelize(process.env[config.use_env_variable]);
-// } else if (process.env.JAWSDB_URL) {
-//   var sequelize = new Sequelize(`mysql:${process.env.JAWSDB_URL}`);
-// } else {
-//   var sequelize = new Sequelize(config.database, config.username, config.password, config);
-// }
-var sequelize = new Sequelize('mysql://i0q6skcxum4q5cqx:m3n18v0yooulsy7l@ol5tz0yvwp930510.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/odgdf1fi2e1ebxwi');
+if (config.use_env_variable) {
+  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+} else if (process.env.JAWSDB_URL) {
+  var sequelize = new Sequelize(`mysql:${process.env.JAWSDB_URL}`);
+} else {
+  var sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
 
 fs
   .readdirSync(__dirname)
