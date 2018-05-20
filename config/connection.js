5f43274a8a88bce7d04e2486f50b46086bd9ee9d
Jeff Nabors <jeffnaborsdev@gmail.com>
2018-05-20 19:21:36 -0400

    Switched database to Postgresql

--- config/connection.js
+++ config/connection.js
@@ -3,13 +3,13 @@ require('dotenv').config();
 module.exports = {
     development: {
         database: process.env.DB_NAME,
-        username: 'root',
+        username: 'postgres',
         password: process.env.DB_PASS,
         host: '127.0.0.1',
-        dialect: 'mysql'
-    },
-    production: {
-        use_env_variable: 'JAWSDB_URL',
-        dialect: 'mysql'
+        dialect: 'postgres'
     }
+    // production: {
+        // use_env_variable: 'JAWSDB_URL',
+        // dialect: 'mysql'
+//     }
 }
